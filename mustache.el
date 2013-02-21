5790292e543429791190fa3912be9770a11a63aa
Wilfred Hughes <me@wilfred.me.uk>
2013-02-21 14:17:51 +0000

    splitting out lexing

--- mustache.el
+++ mustache.el
@@ -1,34 +1,50 @@
 (require 'ht)
 (require 's)
+(require 'dash)
+
+(eval-when-compile '(require 'cl))
 
 ;; todo: add flag to set tolerance of missings variables
 (defun mustache-render (template context)
   "Render a mustache TEMPLATE with hash table CONTEXT."
+  (let ((rendered ""))
+    (dolist (lexeme (mustache-lex template) rendered)
+      (destructuring-bind (type value) lexeme
+        (if (eq type :text)
+            (setq rendered (s-prepend rendered value))
+          (setq rendered (s-prepend rendered (mustache-render-block value context))))))))
+
+(defun mustache-lex (template)
+  "Iterate through TEMPLATE, splitting {{ blocks }} and bare strings.
+We return a list of lists: ((:text \"foo\") (:block \"variable-name\"))"
   (let ((open-delimeter "{{")
         (close-delimeter "}}")
-        (rendered ""))
+        (lexemes nil))
     (while (not (s-equals? template ""))
       (let* ((open-index (s-index-of open-delimeter template))
              (close-index (s-index-of close-delimeter template)))
+        ;; todo: check open-index < close-index
+        ;; todo: error if we have an open and no close
         (if (and open-index close-index)
-            ;; we have something to render
+            ;; we have a block
             (let ((between-delimeters
                    (substring template (+ open-index (length open-delimeter)) close-index))
                   (continue-from-index (+ close-index (length close-delimeter))))
-              ;; append the string before the delimeter
-              (setq rendered
-                    (s-prepend rendered (substring template 0 open-index)))
-              ;; render whatever we have between the delimeters
-              (setq rendered
-                    (s-prepend rendered (mustache-render-block between-delimeters context)))
+              ;; save the string before the block
+              (when (> open-index 0)
+                  (!cons (list :text (substring template 0 open-index)) lexemes))
+              
+              ;; save this block
+              (!cons (list :block between-delimeters) lexemes)
+              
               ;; iterate on the remaining template
               (setq template
                     (substring template continue-from-index)))
           ;; else only plain text left
           (progn
-            (setq rendered (s-prepend rendered template))
+            (!cons (list :text template) lexemes)
             (setq template "")))))
-    rendered))
+    (nreverse lexemes)))
 
 (defun mustache-render-block (between-delimeters context)
   "Given BETWEEN-DELIMETERS text, render it in hash table CONTEXT."
