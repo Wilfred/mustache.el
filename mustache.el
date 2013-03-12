2d4aeb0ae3a67197251ba9a8df89deedbbb718f6
Wilfred Hughes <me@wilfred.me.uk>
2013-03-12 13:53:07 +0000

    adding package-requires

--- mustache.el
+++ mustache.el
@@ -5,6 +5,7 @@
 ;; Author: Wilfred Hughes <me@wilfred.me.uk>
 ;; Version: 0.3
 ;; Keywords: mustache, template
+;; Package-Requires: ((ht "0.7") (s "1.3.0") (dash "1.1.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
