df00bf9b6ce5bf995149d21a77ab3a9363550a0f
Wilfred Hughes <me@wilfred.me.uk>
2013-04-11 00:20:13 +0100

    simplifying context creation using the ht macro

--- mustache-tests.el
+++ mustache-tests.el
@@ -6,8 +6,7 @@
   (should (equal "foo" (mustache-render "foo" nil))))
 
 (ert-deftest mustache-test-variable ()
-  (let ((context (ht-create)))
-    (ht-set context "blah" "bar")
+  (let ((context (ht ("blah" "bar"))))
     (should
      (equal
       "foo bar"
@@ -21,49 +20,43 @@
       (mustache-render "foo {{blah}}" context)))))
 
 (ert-deftest mustache-test-variable-escaped ()
-  (let ((context (ht-create)))
-    (ht-set context "blah" "<bar> &baz ' \"")
+  (let ((context (ht ("blah" "<bar> &baz ' \""))))
     (should
      (equal
       "&lt;bar&gt; &amp;baz &#39; &quot;"
       (mustache-render "{{blah}}" context)))))
 
 (ert-deftest mustache-test-section ()
-  (let ((context (ht-create)))
-    (ht-set context "users" (list (ht-from-plist '("name" "bob"))
-                                  (ht-from-plist '("name" "chris"))))
+  (let ((context (ht ("users" (list (ht ("name" "bob"))
+                                    (ht ("name" "chris")))))))
     (should
      (equal
       "bobchris"
       (mustache-render "{{#users}}{{name}}{{/users}}" context)))))
 
 (ert-deftest mustache-test-unescaped ()
-  (let ((context (ht-create)))
-    (ht-set context "blah" "<bar>")
+  (let ((context (ht ("blah" "<bar>"))))
     (should
      (equal
       "<bar>"
       (mustache-render "{{& blah}}" context)))))
 
 (ert-deftest mustache-test-conditional-true ()
-  (let ((context (ht-create)))
-    (ht-set context "yes" 't)
+  (let ((context (ht ("yes" 't))))
     (should
      (equal
       "foo bar"
       (mustache-render "foo {{#yes}}bar{{/yes}}" context)))))
 
 (ert-deftest mustache-test-conditional-false ()
-  (let ((context (ht-create)))
-    (ht-set context "no" nil)
+  (let ((context (ht ("no" nil))))
     (should
      (equal
       "foo "
       (mustache-render "foo {{#no}}bar{{/no}}" context)))))
 
 (ert-deftest mustache-test-inverted ()
-  (let ((context (ht-create)))
-    (ht-set context "no" nil)
+  (let ((context (ht ("no" nil))))
     (should
      (equal
       "foo bar"
@@ -71,7 +64,6 @@
 
 (ert-deftest mustache-test-comment ()
   (let ((context (ht-create)))
-    (ht-set context "no" nil)
     (should
      (equal
       ""
