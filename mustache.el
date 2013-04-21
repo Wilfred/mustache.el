b0312e943425b5a24516cc82656dc0211a301ce1
Wilfred Hughes <me@wilfred.me.uk>
2013-04-22 00:00:40 +0100

    updating a require comment

--- mustache.el
+++ mustache.el
@@ -40,7 +40,7 @@
 (require 'dash)
 (require 'with-namespace)
 
-(eval-when-compile '(require 'cl)) ;; destructuring-bind
+(eval-when-compile '(require 'cl)) ;; destructuring-bind, loop, return
 
 (with-namespace "mustache"
   ;; todo: add flag to set tolerance of missing variables
