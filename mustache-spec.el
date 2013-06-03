1285ffa98153efae9128d7809dd8926eac5765c7
Wilfred Hughes <me@wilfred.me.uk>
2013-06-03 22:51:45 +0100

    basic running of specification tests

--- mustache-spec.el
+++ mustache-spec.el
@@ -1,17 +1,38 @@
 (require 'json)
 (require 'ht)
+(require 'dash)
+(require 'mustache)
+(eval-when-compile (require 'cl))
 
 (defun read-json-path (path)
   "Open the JSON file at PATH, parse it, and return a hash table of its contents."
   (let ((json-object-type 'hash-table)
-        (json-array-type 'list))
+        (json-array-type 'list)
+        (json-false nil))
     (with-temp-buffer
       (insert-file-contents-literally path)
       (json-read))))
 
 (defun tests-from-path (path)
   "Read tests from a mustache specification file at PATH."
-  (ht-get (read-json-path path) "tests"))
+  (--map (list (ht-get it "data")
+               (ht-get it "template")
+               (ht-get it "expected")
+               )
+         (ht-get (read-json-path path) "tests")))
+
+;; todo: proper ERT unit tests
+(defun* run-tests-from-path (path &aux (failures 0))
+  "Loop over the assertions, message when they fail"
+  (loop for (context template expected)
+        in (tests-from-path path)
+        do (let ((actual (mustache-render template context)))
+             (unless (equal actual expected)
+               (message "Expected %s but got %s" expected actual)
+               (incf failures))))
+  (message "%d failure(s)" failures))
+
+(run-tests-from-path "spec/inverted.json")
 
 (defun mustache-test-spec ()
   (interactive)
