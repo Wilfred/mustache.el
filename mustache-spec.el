68ad1f508403ee2a282d173d71a0f8747b43b93f
Wilfred Hughes <me@wilfred.me.uk>
2013-06-14 23:24:21 +0100

    make the byte compiler happy by marking functions used in macros with eval-and-compile

--- mustache-spec.el
+++ mustache-spec.el
@@ -6,29 +6,30 @@
 (require 's)
 (eval-when-compile (require 'cl))
 
-(defun mst--read-json-path (path)
-  "Open the JSON file at PATH, parse it, and return a hash table of its contents."
-  (let ((json-object-type 'hash-table)
-        (json-array-type 'list)
-        (json-false nil))
-    (with-temp-buffer
-      (insert-file-contents-literally path)
-      (json-read))))
+(eval-and-compile
+  (defun mst--read-json-path (path)
+    "Open the JSON file at PATH, parse it, and return a hash table of its contents."
+    (let ((json-object-type 'hash-table)
+          (json-array-type 'list)
+          (json-false nil))
+      (with-temp-buffer
+        (insert-file-contents-literally path)
+        (json-read))))
 
-(defun mst--ert-from-spec (spec-case)
-  "Return a quoted ert test from a hash-table SPEC-CASE
+  (defun mst--ert-from-spec (spec-case)
+    "Return a quoted ert test from a hash-table SPEC-CASE
 describing expected behaviour"
-  (let* ((spec-name (ht-get spec-case "name"))
-         (test-name (intern (format "mustache-spec-%s" (s-replace " " "-" spec-name))))
-         (description (ht-get spec-case "desc"))
-         (template (ht-get spec-case "template"))
-         (context (ht-get spec-case "data"))
-         (expected (ht-get spec-case "expected")))
-    `(ert-deftest ,test-name ()
+    (let* ((spec-name (ht-get spec-case "name"))
+           (test-name (intern (format "mustache-spec-%s" (s-replace " " "-" spec-name))))
+           (description (ht-get spec-case "desc"))
+           (template (ht-get spec-case "template"))
+           (context (ht-get spec-case "data"))
+           (expected (ht-get spec-case "expected")))
+      `(ert-deftest ,test-name ()
          ,description
-       (should
-        (equal
-         ,expected (mustache-render ,template ,context))))))
+         (should
+          (equal
+           ,expected (mustache-render ,template ,context)))))))
 
 (defmacro ert-tests-from-path (path)
   "Open the JSON file at PATH, and return a list of ert test cases."
